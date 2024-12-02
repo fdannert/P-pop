502ef18fdeb450f521d2d0ce8e9d686b08214fd1
fdannert <62441325+fdannert@users.noreply.github.com>
2024-12-02 13:41:45 +0100

    implemented starcat_v4

--- P-pop.py
+++ P-pop.py
@@ -19,7 +19,7 @@
 
 # Don't print annoying warnings. Comment out if you want to see them.
 import warnings
-#warnings.filterwarnings('ignore')
+warnings.filterwarnings('ignore')
 
 
 # =============================================================================
@@ -28,28 +28,17 @@ import warnings
 
 # Import your own catalogs, distributions and models here.
 import SystemGenerator
-from StarCatalogs import CrossfieldBrightSample, ExoCat_1, LTC_2, LTC_3
-from PlanetDistributions import Fressin2013,\
-                                Burke2015,\
-                                Dressing2015,\
-                                SAG13,\
-                                Weiss2018,\
-                                Weiss2018KDE,\
-                                HabitableNominal,\
-                                HabitablePessimistic,\
-                                Fernandes2019Symm#,\
-                                #Bryson2021Model1Hab2Low,\
-                                #Bryson2021Model1Hab2High
+from StarCatalogs import CrossfieldBrightSample, ExoCat_1, LTC_2, LTC_3, LTC_4, HabWorldsCat
+from PlanetDistributions import Fressin2013, Burke2015, Dressing2015, SAG13,\
+                                Weiss2018, Weiss2018KDE, HabitableNominal, \
+                                HabitablePessimistic
 from ScalingModels import BinarySuppression
 from MassModels import Chen2017
 from EccentricityModels import Circular
 from StabilityModels import He2019
 from OrbitModels import Random
-from AlbedoModels import Uniform,\
-                         Constant
-from ExozodiModels import Ertel2018,\
-                          Ertel2020,\
-                          Median
+from AlbedoModels import Uniform
+from ExozodiModels import Ertel2020, Ertel2018
 
 
 # =============================================================================
@@ -60,13 +49,14 @@ from ExozodiModels import Ertel2018,\
 
 # Select the star catalog, the spectral types, the distance range and the
 # declination range which should be included here.
-# StarCatalog = CrossfieldBrightSample # used in Kammerer & Quanz 2018
-# StarCatalog = ExoCat_1 # used by NASA
-# StarCatalog = LTC_2 # LIFE Target Catalog (version 2)
-StarCatalog = LTC_3 # LIFE Target Catalog (version 3)
+#StarCatalog = CrossfieldBrightSample # used in Kammerer & Quanz 2018
+#StarCatalog = ExoCat_1 # used by NASA
+#StarCatalog = LTC_2 # LIFE Target Catalog (version 2)
+StarCatalog = LTC_4 # LIFE Target Catalog (version 3)
 Stypes = ['A', 'F', 'G', 'K', 'M'] # list of str
-Dist_range = [0., 20.] # pc, list of float, [min, max]
+Dist_range = [0., 30.] # pc, list of float, [min, max]
 Dec_range = [-90., 90.] # deg, list of float, [min, max]
+
 # Teff_range = [4800., 6300.] # K, list of float, [min, max]
 Teff_range = None
 
@@ -75,23 +65,20 @@ Teff_range = None
 # spectral type.
 # dict, StarCatalog.Stype as keys, PlanetDistribution as data
 # StypeToModel = {'A': Fressin2013, 'F': Fressin2013, 'G': Fressin2013, 'K': Fressin2013, 'M': Fressin2013}
-# StypeToModel = {'F': Burke2015, 'G': Burke2015, 'K': Burke2015, 'M': Dressing2015}
-StypeToModel = {'F': SAG13, 'G': SAG13, 'K': SAG13, 'M': Dressing2015} # used in the ESA Voyage 2050 White Paper
-# StypeToModel = {'F': SAG13, 'G': SAG13, 'K': SAG13, 'M': SAG13}
-# StypeToModel = {'F': Weiss2018, 'G': Weiss2018, 'K': Weiss2018, 'M': Weiss2018}
-# StypeToModel = {'F': Weiss2018KDE, 'G': Weiss2018KDE, 'K': Weiss2018KDE, 'M': Weiss2018KDE}
-# StypeToModel = {'A': SAG13, 'F': SAG13, 'G': SAG13, 'K': SAG13, 'M': Dressing2015}
+#StypeToModel = {'F': Burke2015, 'G': Burke2015, 'K': Burke2015, 'M': Dressing2015}
+#StypeToModel = {'F': SAG13, 'G': SAG13, 'K': SAG13, 'M': Dressing2015} # used in the ESA Voyage 2050 White Paper
+#StypeToModel = {'F': SAG13, 'G': SAG13, 'K': SAG13, 'M': SAG13}
+#StypeToModel = {'F': Weiss2018, 'G': Weiss2018, 'K': Weiss2018, 'M': Weiss2018}
+#StypeToModel = {'F': Weiss2018KDE, 'G': Weiss2018KDE, 'K': Weiss2018KDE, 'M': Weiss2018KDE}
+#StypeToModel = {'A': SAG13, 'F': SAG13, 'G': SAG13, 'K': SAG13, 'M': Dressing2015}
 # StypeToModel = {'A': SAG13, 'F': SAG13, 'G': SAG13, 'K': SAG13, 'M': SAG13}
-# StypeToModel = {'A': HabitableNominal, 'F': HabitableNominal, 'G': HabitableNominal, 'K': HabitableNominal, 'M': HabitableNominal}
-# StypeToModel = {'A': HabitablePessimistic, 'F': HabitablePessimistic, 'G': HabitablePessimistic, 'K': HabitablePessimistic, 'M': HabitablePessimistic}
-# StypeToModel = {'A': Fernandes2019Symm, 'F': Fernandes2019Symm, 'G': Fernandes2019Symm, 'K': Fernandes2019Symm, 'M': Fernandes2019Symm}
-# StypeToModel = {'A': Bryson2021Model1Hab2Low, 'F': Bryson2021Model1Hab2Low, 'G': Bryson2021Model1Hab2Low, 'K': Bryson2021Model1Hab2Low, 'M': Bryson2021Model1Hab2Low}
-# StypeToModel = {'A': Bryson2021Model1Hab2High, 'F': Bryson2021Model1Hab2High, 'G': Bryson2021Model1Hab2High, 'K': Bryson2021Model1Hab2High, 'M': Bryson2021Model1Hab2High}
+StypeToModel = {'A': HabitableNominal, 'F': HabitableNominal, 'G': HabitableNominal, 'K': HabitableNominal, 'M': HabitableNominal}
+#StypeToModel = {'A': HabitablePessimistic, 'F': HabitablePessimistic, 'G': HabitablePessimistic, 'K': HabitablePessimistic, 'M': HabitablePessimistic}
+#StypeToModel = {'A': Fernandes2019symm, 'F': Fernandes2019symm, 'G': Fernandes2019symm, 'K': Fernandes2019symm, 'M': Fernandes2019symm}
 Scenario = 'baseline'
-# Scenario = 'pessimistic' # for 1-sigma lower error bars on planet distribution
-# Scenario = 'optimistic' # for 1-sigma upper error bars on planet distribution
-# Scenario = 'mc' # draw parameters from MCMC posterior for each universe
-# ScalingModel = None
+#Scenario = 'pessimistic' # for 1-sigma lower error bars on planet distribution
+#Scenario = 'optimistic' # for 1-sigma upper error bars on planet distribution
+#ScalingModel = None
 ScalingModel = BinarySuppression
 
 # Select the mass model, the eccentricity model, the stability model, the orbit
@@ -99,31 +86,29 @@ ScalingModel = BinarySuppression
 # here.
 MassModel = Chen2017 # Forecaster
 EccentricityModel = Circular
-# StabilityModel = None
+#StabilityModel = None
 StabilityModel = He2019
 OrbitModel = Random
 AlbedoModel = Uniform
-# AlbedoModel = Constant
-# ExozodiModel = Ertel2018
+#ExozodiModel = Ertel2018
 ExozodiModel = Ertel2020
-# ExozodiModel = Median
 
 # Select whether you want to display summary plots after loading the catalogs,
 # distributions and models selected above, how many test draws should be
 # done for generating these plots, and where you want to save them.
-# SummaryPlots = True
-SummaryPlots = False
+SummaryPlots = True
+#SummaryPlots = False
 Ntest = 100000 # int
-# Ntest = 10000 # int
-# FigDir = None # if you don't want to save the summary plots
-FigDir = 'Figures_TestPlanetPopulation/' # str, should end with a slash ("/")
-# block = True
+#Ntest = 10000 # int
+#FigDir = None # if you don't want to save the summary plots
+FigDir = 'Figures/' # should end with a slash ("/")
+#block = True
 block = False
 
 # Select a name for the output planet population table and how many universes
 # should be simulated.
-Name = 'white_paper_5uni_names' # str
-Nuniverses = 5 # int
+Name = 'LTC4_habnom_10' # str
+Nuniverses = 10 # int
 
 
 # =============================================================================
@@ -143,7 +128,7 @@ SysGen = SystemGenerator.SystemGenerator(StarCatalog,
                                          Stypes,
                                          Dist_range, # pc
                                          Dec_range, # deg
-                                         Teff_range, # K
+                                         Teff_range,
                                          Scenario,
                                          SummaryPlots,
                                          Ntest,
